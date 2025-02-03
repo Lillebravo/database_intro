74e277ae5d8899f3a07fd155552465d07702479c
Jerry <you@example.com>
2025-02-03 10:25:33 +0100

    added get all customers function

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-const Database = require('better-sqlite3');
-const db = new Database('test.db', {verbose: console.log});
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database('test.db', (err) => {
+    if (err) {
+        console.error("couldn´t connect to database", err);
+        return;
+    }
+    console.log("connected to database");
+});
+
+/* const Database = require("better-sqlite3");
+const db = new Database("test.db", { verbose: console.log }); */
 
 function addCustomer(name, mail, phoneNumber, deliveryAddress) {
-    try {
-        const stmt = db.prepare('INSERT INTO customer (customerName, customerEmail, customerPhoneNumber, customerDeliveryAdress) VALUES (?,?,?,?)');
-        const info = stmt.run(name, mail, phoneNumber, deliveryAddress);
-        console.log(`user added with id: ${info.lastInsertRowid}`);
-    } catch (err) {
-        console.error("Couldn´t add new customer: ", err);
-    }
+  try {
+    const stmt = db.prepare(
+      "INSERT INTO customer (customerName, customerEmail, customerPhoneNumber, customerDeliveryAdress) VALUES (?,?,?,?)"
+    );
+    const info = stmt.run(name, mail, phoneNumber, deliveryAddress);
+    console.log(`user added with id: ${info.lastInsertRowid}`);
+  } catch (err) {
+    console.error("Couldn´t add new customer: ", err);
+  }
 }
 
-addCustomer("Jerry", "abc@gmail.com", "0707-123456", "Anderssons väg 7");
+const getAllCustomers = () => {}
+
+db.all("SELECT * FROM customer", [], (err, rows) => {
+  if (err) {
+    console.error("Couldn´t show customers: ", err);
+    return;
+  }
+  console.log("Alla kunder: ");
+  rows.forEach((customer) => {
+    console.log(
+      `ID: ${customer.customerId}, Namn: ${customer.customerName}, Email: ${customer.customerEmail}, Phone number: ${customer.customerPhoneNumber}, Delivery address: ${customer.customerDeliveryAdress}`
+    );
+  });
+});
+
+/* addCustomer("Jerry", "abc@gmail.com", "0707-123456", "Anderssons väg 7");
+ */
